bdf66ab9fbbc897502819988b5afb65255a98574
Herbert Veitengruber <tohklidan@hotmail.com>
2013-05-13 23:53:22 +0200

    cleanup

--- component/IChangeableContentComponent.java
+++ component/IChangeableContentComponent.java
@@ -1,5 +1,28 @@
+/*
+ *  __  __      
+ * /\ \/\ \  __________   
+ * \ \ \_\ \/_______  /\   
+ *  \ \  _  \  ____/ / /  
+ *   \ \_\ \_\ \ \/ / / 
+ *    \/_/\/_/\ \ \/ /  
+ *             \ \  /
+ *              \_\/
+ *
+ * -----------------------------------------------------------------------------
+ * @author: Herbert Veitengruber 
+ * @version: 1.0.0
+ * -----------------------------------------------------------------------------
+ *
+ * Copyright (c) 2013 Herbert Veitengruber 
+ *
+ * Licensed under the MIT license:
+ * http://www.opensource.org/licenses/mit-license.php
+ */
 package jhv.component;
 
+/**
+ * Interface for flagging components that contain unsaved content.
+ */
 public interface IChangeableContentComponent
 {
 	/**
